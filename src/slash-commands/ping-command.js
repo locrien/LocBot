1ff87c26f3486d48f8ce62f44b37f8b8c3f42bd5
LOC-SP\sarth <filion.jonathan@gmail.com>
2023-03-29 12:26:10 -0400

    fix br nomenclature

--- src/slash-commands/ping-command.js
+++ src/slash-commands/ping-command.js
@@ -4,25 +4,25 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setNameLocalizations({
-            PortugueseBR:'ping',
+            "pt-BR":'ping',
             fr:'ping'
         })
         .setDescription('Replies with Pong!')
         .setDescriptionLocalizations({
-            PortugueseBR:'Responde com pong!',
+            "pt-BR":'Responde com pong!',
             fr:'Reponds avec Pong!'
         })
         .addBooleanOption(option =>
-            option.setName('secret')
-                .setNameLocalizations({
-                    PortugueseBR:'segredo',
-                    fr:'caché'
-                })
-                .setDescription('only you can see result?'))
-                .setDescriptionLocalizations({
-                    PortugueseBR:'segredo',
-                    fr:'caché'
-                }),
+        option.setName('secret')
+            .setNameLocalizations({
+                "pt-BR":'segredo',
+                fr:'caché'
+            })
+            .setDescription('only you can see result?'))
+            .setDescriptionLocalizations({
+                "pt-BR":'segredo',
+                fr:'caché'
+            }),
     async execute(interaction) {
         const isSecret = interaction.options.getBoolean('secret');
         await interaction.reply({content:'Pong!',ephemeral:isSecret});
